169070d0082e61a301813d8d7b889004ca024bc6
gopi <gopi@hike.in>
2018-05-05 15:12:50 +0530

    easing functions.

--- reactCode/FirstAnimation.js
+++ reactCode/FirstAnimation.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Text, View, TextInput, Button, Alert, Animated, Easing } from 'react-native';
+import {Text, View, TextInput, Button, Alert, Animated, Easing, TouchableHighlight } from 'react-native';
 
 export default class FirstAnimation extends Component {
     static navigationOptions = {
@@ -12,6 +12,9 @@ export default class FirstAnimation extends Component {
             firstAnim: new Animated.Value(10),
             secondAnim: new Animated.Value(1)
         };
+        this.animatedValue1 = new Animated.Value(0);
+        this.animatedValue2 = new Animated.Value(0);
+        this.animatedValue3 = new Animated.Value(0);
     }
 
 
@@ -21,6 +24,28 @@ export default class FirstAnimation extends Component {
             easing: Easing.bounce,  // easing is a function pass with bind
             duration: 2000
         }).start();
+    }
+
+    animate () {
+        this.animatedValue1.setValue(0);
+        this.animatedValue2.setValue(0);
+        this.animatedValue3.setValue(0);
+        const createAnimation = function (value, duration, easing, delay = 0) {
+            return Animated.timing(
+                value,
+                {
+                    toValue: 1,
+                    duration,
+                    easing,
+                    delay
+                }
+            )
+        };
+        Animated.parallel([
+            createAnimation(this.animatedValue1, 2000, Easing.ease),
+            createAnimation(this.animatedValue2, 1000, Easing.ease, 1000),
+            createAnimation(this.animatedValue3, 1000, Easing.ease, 2000)
+        ]).start()
     }
 
 
@@ -76,6 +101,19 @@ export default class FirstAnimation extends Component {
             ]
         };
 
+        const scaleText = this.animatedValue1.interpolate({
+            inputRange: [0, 1],
+            outputRange: [0.5, 2]
+        });
+        const spinText = this.animatedValue2.interpolate({
+            inputRange: [0, 1],
+            outputRange: ['0deg', '720deg']
+        });
+        const introButton = this.animatedValue3.interpolate({
+            inputRange: [0, 1],
+            outputRange: [-100, 400]
+        });
+
 
         return (
             <View>
@@ -86,6 +124,29 @@ export default class FirstAnimation extends Component {
 
                 <Animated.View style={{width: 50, height: 50, backgroundColor: 'red', opacity: alphaInterpolate}}/>
                 <Animated.View style={[{width: 50, height: 100, backgroundColor: 'green'}, rotate, scale ]}/>
+
+                <Animated.View
+                    style={{ transform: [{scale: scaleText}] }}>
+                    <Text>Welcome</Text>
+                </Animated.View>
+                <Animated.View
+                    style={{ marginTop: 20, transform: [{rotate: spinText}] }}>
+                    <Text
+                        style={{fontSize: 20}}>
+                        to the App!
+                    </Text>
+                </Animated.View>
+                <Animated.View
+                    style={{top: introButton, position: 'absolute'}}>
+                    <TouchableHighlight
+                        onPress={this.animate.bind(this)}
+                        >
+                        <Text
+                            style={{color: 'white', fontSize: 20}}>
+                            Click Here To Start
+                        </Text>
+                    </TouchableHighlight>
+                </Animated.View>
             </View>
 
         );
