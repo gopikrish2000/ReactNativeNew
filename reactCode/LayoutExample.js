c890ff4f7b3a81a664256b7d726270bcc673af44
gopi <gopi@hike.in>
2018-05-06 15:07:30 +0530

    aligning 4 elements to 4 different corners.

--- reactCode/LayoutExample.js
+++ reactCode/LayoutExample.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
-import {Text, View, TextInput, Button, Alert, Animated, Easing, TouchableHighlight, StyleSheet} from 'react-native';
+import {Text, View,Dimensions, TextInput, Button, Alert, Animated, Easing, TouchableHighlight, StyleSheet} from 'react-native';
 
 
 export default class LayoutExample extends Component {
 
-    functionToExecute = 1;
+    functionToExecute = 3;
     constructor(props) {
         super(props);
     }
@@ -29,17 +29,45 @@ export default class LayoutExample extends Component {
         );
     }
 
-    secondExample(){
+    secondExample(){ // align 3 elements middle is center & 1 is left of 2 ; 3 is right of 2 ... ;; Each divide into multiple flexboxes n align it.
         return (
             <View style={styles.container}>
                 <View style={styles.component1}>
                     <Text>Right</Text>
                 </View>
                 <View style={styles.component2}>
-                    <Text style={{ textAlign:'center' }}>Center Component</Text>
+                    <Text style={{ }}>Center Component1</Text>
+                    <Text style={{ marginTop: 30 }}>Center Component2</Text>
+                    <Text style={{ }}>Center Component3</Text>
                 </View>
                 <View style={styles.component3}>
                     <Text> component3 </Text>
+                </View>
+            </View>
+        );
+    }
+
+
+    thirdExample(){ // align 4 elements across 4 corners topLeft , topRight , BottomLeft , BottomRight ;
+                    // for this the parent Width, Height should be given & position: relative
+        lWidth = Dimensions.get('window').width;
+        lHeight = Dimensions.get('window').height - 100;
+
+        return (
+            <View style={{position:'relative', width:lWidth, height: lHeight}}>
+                <View style={{position: 'absolute', top: 0 , left: 0}}>
+                    <Text>Right</Text>
+                </View>
+                <View style={{position: 'absolute', top: 0 , right: 0}}>
+                    <Text style={{ }}>Center Component1</Text>
+                    <Text style={{ marginTop: 30 }}>Center Component2</Text>
+                    <Text style={{ }}>Center Component3</Text>
+                </View>
+                <View style={{position: 'absolute', bottom: 0 , left: 0}}>
+                    <Text> component3 </Text>
+                </View>
+                <View style={{position: 'absolute', bottom: 0 , right: 0}}>
+                    <Text> component4 </Text>
                 </View>
             </View>
         );
@@ -50,6 +78,8 @@ export default class LayoutExample extends Component {
             return this.firstExample();
         } else if ( this.functionToExecute === 2){
             return this.secondExample();
+        } else if ( this.functionToExecute === 3){
+            return this.thirdExample();
         }
     }
 };
