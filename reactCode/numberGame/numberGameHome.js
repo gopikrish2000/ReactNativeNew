2a52843e5188f5f04f904448bf73da9f2c8ca30e
gopi <gopi@hike.in>
2018-05-08 00:23:02 +0530

    sample list added to the game.

--- reactCode/numberGame/numberGameHome.js
+++ reactCode/numberGame/numberGameHome.js
@@ -1,15 +1,16 @@
 import React, {Component} from 'react';
-import {Text, View, TextInput, Button, Alert, Animated, Easing, TouchableHighlight} from 'react-native';
+import {Text, View,FlatList, TextInput, Button, Alert, Animated, Easing, TouchableHighlight} from 'react-native';
 
 
 export default class NumberGameHome extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {counDownIndex: 15};
+        this.state = {counDownIndex: 15, dataList:[]};
     }
 
     componentDidMount(){
+        this.fetchFromNetwork();
         this.intervalId = setInterval(function(){
             if(this.state.counDownIndex < 1) {
                 clearInterval(this.intervalId);
@@ -21,17 +22,38 @@ export default class NumberGameHome extends Component {
         }.bind(this), 1000);
     }
 
+    fetchFromNetwork() {
+        fetch("https://facebook.github.io/react-native/movies.json").then((response) => response.json()).then((jsonObj) => jsonObj["movies"]).then((json) => {
+            // console.log(" json is " + json);
+            this.setState({
+                dataList: json
+            }, () => {
+
+            });
+        }).catch((exception) => {
+            console.error(exception);
+        });
+    }
+
     render(): * {
         const pinkOpacity = this.state.counDownIndex <= 0 ? 0 : 1;
         return (
-            <View style={{height:400, width:600}}>
-                <View style={{flex:1, flexDirection:'row'}}>
+            <View style={{flex:1, flexDirection:'column', justifyContent:'flex-start'  }}>
+                <View style={{opacity:pinkOpacity ,flex:1, flexDirection:'row', marginLeft:40, marginTop:20, marginBottom:30}}>
                     <Text>Game start in countdown of </Text>
-                    { <View style={{opacity:pinkOpacity , backgroundColor:'blue', height: 40, width:40 , borderRadius:20, }}>
+                    { <View style={{ backgroundColor:'blue', height: 40, width:40 , borderRadius:20, }}>
                         <View style={{flex:1, justifyContent: 'center'}}>
                             <Text style={{color:'red', fontSize:20, alignSelf:'center'}}> {this.state.counDownIndex} </Text>
                         </View>
                     </View>}
+                </View>
+
+                <View>
+                    <FlatList data={this.state.dataList} keyExtractor={(item, index) => index + ""}
+                              renderItem={({item}) => <Text> movie tilte is {item.title} , release year
+                                  is {item.releaseYear} </Text>}
+                    />
+
                 </View>
             </View>
         );
